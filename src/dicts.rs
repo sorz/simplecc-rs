5f8549c2a5cb3a056505651db2fd7fcf0f36da45
sorz <orz@sorz.org>
2018-03-05 16:25:36 +1100

    Refactor: generate built-in dict with macros

--- src/dicts.rs
+++ src/dicts.rs
@@ -3,23 +3,31 @@
 //! Turn on `builtin_dicts` feature to enable them.
 use Dict;
 
+macro_rules! builtin_dicts {
+    ( $x:expr, $( $y:expr ),* ) => {
+        {
+            let lines = include_str!(
+                concat!("../OpenCC/data/dictionary/", $x, ".txt")).lines();
+            let mut lines = Box::new(lines) as Box<Iterator<Item=&'static str>>;
+            $(
+                let text = include_str!(
+                    concat!("../OpenCC/data/dictionary/", $y, ".txt"));
+                lines = Box::new(lines.chain(text.lines()));
+            )*
+            Dict::load_lines(lines)
+        }
+    };
+}
+
 lazy_static! {
     /// Simplified Chinese to Traditional Chinese
     pub static ref S2T: Dict = {
-        let dict = concat!(
-            include_str!("../OpenCC/data/dictionary/STCharacters.txt"),
-            include_str!("../OpenCC/data/dictionary/STPhrases.txt"),
-        );
-        Dict::load_str(dict)
+        builtin_dicts!("STCharacters", "STPhrases")
     };
 
     /// Traditional Chinese to Simplified Chinese
     pub static ref T2S: Dict = {
-        let dict = concat!(
-            include_str!("../OpenCC/data/dictionary/TSCharacters.txt"),
-            include_str!("../OpenCC/data/dictionary/TSPhrases.txt"),
-        );
-        Dict::load_str(dict)
+        builtin_dicts!("TSCharacters", "TSPhrases")
     };
 
 }
